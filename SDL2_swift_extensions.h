c89f8dcf09fa278875fbbe0e85f4af55d51505cb
Jason Frame <jason@onehackoranother.com>
2016-02-05 12:26:30 +0000

    add accessors for pixel format

--- SDL2_swift_extensions.h
+++ SDL2_swift_extensions.h
@@ -2,3 +2,5 @@ int SDL_X_GetSurfaceWidth(SDL_Surface *s);
 int SDL_X_GetSurfaceHeight(SDL_Surface *s);
 int SDL_X_GetSurfacePitch(SDL_Surface *s);
 void* SDL_X_GetSurfacePixels(SDL_Surface *s);
+SDL_PixelFormat* SDL_X_GetSurfacePixelFormat(SDL_Surface *s);
+Uint32 SDL_X_GetPixelFormatFormat(SDL_PixelFormat *f);
--- support/SDL2_swift_extensions.c
+++ support/SDL2_swift_extensions.c
@@ -16,3 +16,11 @@ int SDL_X_GetSurfacePitch(SDL_Surface *s) {
 void* SDL_X_GetSurfacePixels(SDL_Surface *s) {
 	return s->pixels;
 }
+
+SDL_PixelFormat* SDL_X_GetSurfacePixelFormat(SDL_Surface *s) {
+	return s->format;
+}
+
+Uint32 SDL_X_GetPixelFormatFormat(SDL_PixelFormat *f) {
+	return f->format;
+}
